3b9ec00b153eeed0361d89dec1bd1bd853b7e202
Novish Khalil <novishkhalil2@hotmail.com>
2024-11-11 00:34:02 +0500

    Add files via upload

---
+++ Q23.js
@@ -0,0 +1,25 @@
+"use strict";
+//23. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let car1 = 'Honda';
+console.log("Is car == 'Honda'? I predict true");
+console.log(car1 == 'Honda');
+let place = 'England';
+console.log("Is place == 'England'? Ipredict true");
+console.log(place == 'England');
+let pizza = 'Fajita';
+console.log("Is pizza == 'Fajita'? I predict true");
+console.log(pizza == 'Fajita');
+let exams = 'Mid term';
+console.log("Is exams == 'Mid term'? I predict true");
+console.log(exams == 'Mid term');
+let chocolate = 'kitkat';
+console.log("Is chocolate == 'kitkat'? I predict true");
+console.log(chocolate == 'kitkat');
+let month = 'march';
+console.log("Is month == 'march'? I predict true");
+console.log(month == 'march');
